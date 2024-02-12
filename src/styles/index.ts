dc115e1b853bc67c4d981f284ad25db66037f8d0
ViniciuMota <vinimota44@gmail.com>
2024-02-12 18:32:34 -0300

    part 1

---
+++ src/styles/index.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components'
+import { estilo } from './variaveis'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    color: ${estilo.corDaFonte};
+    text-decoration:none;
+  }
+
+  .container {
+    max-width: 1024px;
+    margin: 0 auto;
+  }
+`
+
+export default EstiloGlobal
